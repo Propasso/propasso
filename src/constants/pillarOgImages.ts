6b536664788025c7a697e7981b62f3c38152a5c4
ypzvbcdg8k-gif <hallo@propasso.nl>
2026-03-23 19:45:11 +0100

    Create pillarOgImages.ts
    with og images of the 6 pillars

---
+++ src/constants/pillarOgImages.ts
@@ -0,0 +1,14 @@
+export const pillarOgImages: Record<string, string> = {
+  "bedrijfswaarde-en-winstgevendheid-verbeteren":
+    "https://propasso.nl/og/pillar-bedrijfswaarde-en-winstgevendheid-verbeteren.png",
+  "bedrijf-overdraagbaar-maken":
+    "https://propasso.nl/og/pillar-bedrijf-overdraagbaar-maken.png",
+  "afhankelijkheid-van-de-ondernemer-verminderen":
+    "https://propasso.nl/og/pillar-afhankelijkheid-van-de-ondernemer-verminderen.png",
+  "bedrijfswaardering-en-waardedrijvers":
+    "https://propasso.nl/og/pillar-bedrijfswaardering-en-waardedrijvers.png",
+  "voorbereiding-op-bedrijfsoverdracht":
+    "https://propasso.nl/og/pillar-voorbereiding-op-bedrijfsoverdracht.png",
+  "het-persoonlijke-en-financiele-plan-na-overdracht":
+    "https://propasso.nl/og/pillar-het-persoonlijke-en-financiele-plan-na-overdracht.png",
+};
